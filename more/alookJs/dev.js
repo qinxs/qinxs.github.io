d56095570716524185fff78ea45fcf9357489ab7
qinxs <qin_xs@sina.cn>
2022-12-03 22:41:20 +0800

    Site updated: 2022-12-03 22:41:15

--- more/alookJs/dev.js
+++ more/alookJs/dev.js
@@ -1,11 +1,1 @@
-/*
- * @name: alookUrl测试
- * @Author: qinxs
- * @version: 1.0.0
- * @description: 这是来至url的脚本
- * @include: *
- */
-(function() {
-  alert('url');
-  console.log(333);
-})();
+alert("url"),console.log(333);
